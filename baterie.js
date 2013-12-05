ba054ede23c7ab3a1400e8fe75ed4bbdbdcedd77
Dawid Ostrowski <dos@google.com>
2013-12-05 17:43:22 +0100

    Infowindows

--- baterie.js
+++ baterie.js
@@ -1,24 +1,27 @@
 	var map;
     var markers = [];
+    var currentInfoWindow;
 
     function showMarkers() {
       var bounds = map.getBounds();
       var northeast = bounds.getNorthEast();
       var southwest = bounds.getSouthWest();
-      var endpoint = "https://script.google.com/macros/s/AKfycbx5147iblNol6l2n50x472S2OAwhcIXUvOpir6w1mGIsPliRO4f/exec?f1=" 
-        + southwest.lng() + "&f2=" + southwest.lat() + "&t1=" + northeast.lng() + "&t2=" +  northeast.lat();        
+      var endpoint = "https://script.google.com/macros/s/AKfycbyTbuVRRNvv2LKauRMHlWdRHqYtfSvXPVpahhgaDha0g6q6zX2-/exec?f1=" 
+        + southwest.lng() + "&f2=" + southwest.lat() + "&t1=" + northeast.lng() + "&t2=" +  northeast.lat();    
       
       $.get(endpoint, function( data ) {
+
+        for (var i=0; i<markers.length; i++) {
+          markers[i].setMap(null);
+        }
+        markers = [];
+
         if (data) {
           var locationsList = "";
-          for (var i=0; i<markers.length; i++) {
-            markers[i].setMap(null);
-          }
-          markers = [];
           
           for (var i=0; i<data.length; i++) {
             var location = data[i];
-            var locationTag = "<div class='location'>" + location.name + "</div>";
+            var locationTag = "<div class='location'>" + location.name +" - " + location.address + "</div>";
 
             var position = new google.maps.LatLng(location.lat, location.lng);
             var marker = new google.maps.Marker({
@@ -27,15 +30,7 @@
               title: location.name
             });
 
-/*
-            var infowindow = new google.maps.InfoWindow({
-                  content: location.name
-            });
-
-            google.maps.event.addListener(marker, 'click', function() {
-              infowindow.open(map,marker);
-            });
-*/
+            google.maps.event.addListener(marker, 'click', createClickFunc(location, marker));
 
             markers.push(marker);
             locationsList = locationsList + " " + locationTag;
@@ -45,6 +40,22 @@
       });
       
     }
+
+    function createClickFunc(location, marker) {
+
+      var infowindow = new google.maps.InfoWindow({
+        content: location.name
+      });
+
+      return function() {
+        if (currentInfoWindow) {
+          currentInfoWindow.close();
+        }
+        infowindow.open(map,marker);
+        currentInfoWindow = infowindow;
+      }
+    }
+
 
       function initialize() {
         var mapOptions = {
